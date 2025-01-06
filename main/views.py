1787f7dd758671af8efa6d78b03c1c71279077b4
kostya362 <ofwer362@gmail.com>
2025-01-06 03:34:29 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -5,12 +5,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'result': False,
-        'title': 'Home',
-        'content': ['Главная страница магазина', 'Страница']
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Пока</h1>')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о магазине'
+    }
+
+    return render(request, 'main/about.html', context)
